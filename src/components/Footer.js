2eff60c9b3e0edc4fe7f75e46e425cf2e6e297e4
Margaux Chevreux <margaux.chevreux@mgxcx.fr>
2021-09-10 11:18:12 +0200

    Changing get in touch in Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,10 +6,7 @@ export default function Footer() {
       <div className="inner">
         <h2 className="major">Get in touch</h2>
         <p>
-          Cras mattis ante fermentum, malesuada neque vitae, eleifend erat.
-          Phasellus non pulvinar erat. Fusce tincidunt, nisl eget mattis
-          egestas, purus ipsum consequat orci, sit amet lobortis lorem lacus in
-          tellus. Sed ac elementum arcu. Quisque placerat auctor laoreet.
+
         </p>
         <form method="post" action="/#">
           <div className="fields">
@@ -38,10 +35,10 @@ export default function Footer() {
           <li className="fa-phone">{config.phone}</li>
 
           {config.socialLinks.map(social => {
-            const { icon, url } = social;
+            const { icon, name, url } = social;
             return (
               <li className={`${icon}`} key={url}>
-                <a href={url}>{url}</a>
+                <a href={url}>{name}</a>
               </li>
             );
           })}
